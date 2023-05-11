1c06a1405290d889a34fba32b69a6b352509ad49
Lucas GILHARD <l.gilhard@gmail.com>
2023-05-11 18:07:50 +0200

    add base TasksService

---
+++ projects/routine/src/app/tasks/tasks.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+
+import { Task } from './task';
+import { TASKS } from './mock-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+  private subject: BehaviorSubject<Task[]>;
+
+  constructor() {
+    this.subject = new BehaviorSubject<Task[]>(TASKS);
+  }
+
+  getTasks(): Observable<Task[]> {
+    return this.subject.asObservable();
+  }
+
+  getTask(id: string): Observable<Task | undefined> {
+    return this.getTasks().pipe(
+      map<Task[], Task | undefined>(tasks => {
+        return tasks.find((task) => task.id === id);
+      })
+    );
+  }
+
+  addTask(task: Task): void {
+    TASKS.push(task);
+    this.subject.next(TASKS);
+  }
+
+  updateTask(task: Task): void {
+    TASKS[TASKS.findIndex((t) => t.id === task.id)] = task;
+    this.subject.next(TASKS);
+  }
+
+  deleteTask(task: Task): void {
+    TASKS.splice(TASKS.findIndex((t) => t.id === task.id), 1);
+    this.subject.next(TASKS);
+  }
+}
